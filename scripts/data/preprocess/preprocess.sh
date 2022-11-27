9877d6859fe9e30555f38804ab2f390bf0720006
Sumire Honda <pu.sumirehonda@gmail.com>
2022-11-27 23:29:09 +0100

    add file

---
+++ scripts/data/preprocess/preprocess.sh
@@ -0,0 +1,29 @@
+VOCAB_SIZE=32000
+
+REPO = C:/Users/hsmr0/IM/main/contextual-mt
+data_dir = C:/Users/hsmr0/IM/main/contextual-mt/scripts/data
+
+for lang in ja en; do
+    python $REPO/scripts/spm_train.py \
+        ${data_dir}/train.${lang} \
+        --model-prefix ${data_dir}/prep/spm.${lang} \
+        --vocab-file ${data_dir}/prep/dict.${lang}.txt \
+        --vocab-size $VOCAB_SIZE
+done
+for split in train valid test; do
+    for lang in ja en; do
+        python $REPO/scripts/spm_encode.py \
+            --model ${data_dir}/prep/spm.$lang.model \
+                < ${data_dir}/${split}.${lang} \
+                > ${data_dir}/prep/${split}.sp.${lang}
+    done
+done
+
+fairseq-preprocess \
+    --source-lang src --target-lang tgt \
+    --trainpref ${data_dir}/prep/train.sp \
+    --validpref ${data_dir}/pep/valid.sp \
+    --testpref ${data_dir}/prep/test.sp \
+    --srcdict ${data_dir}/prep/dict.src.txt \
+    --tgtdict ${data_dir}/prep/dict.tgt.txt \
+    --destdir ${data_dir}/bin
