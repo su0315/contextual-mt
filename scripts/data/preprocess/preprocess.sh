50d55205cce173d20fb1536cf58de42bca78ba2f
Sumire Honda <pu.sumirehonda@gmail.com>
2022-11-30 12:16:37 +0100

    vocab file name modified and src/tgt_lang set

--- scripts/data/preprocess/preprocess.sh
+++ scripts/data/preprocess/preprocess.sh
@@ -3,18 +3,15 @@ VOCAB_SIZE=32000
 REPO=C:/Users/hsmr0/IM/main/contextual-mt
 data_dir=C:/Users/hsmr0/IM/main/contextual-mt/data/BSD-master/for_preprocess
 
-echo $REPO
-
-
-for lang in ja en; do
+for lang in $src_lang $tgt_lang; do
     python $REPO/scripts/spm_train.py \
         ${data_dir}/train.${lang} \
-        --model-prefix ${data_dir}/prep/spm.${lang}.nopretok \ 
-        --vocab-file ${data_dir}/prep/dict.${lang}.txt \
+        --model-prefix ${data_dir}/prep/spm.${lang}.nopretok.model \ 
+        --vocab-file ${data_dir}/prep/spm.${lang}.nopretok.vocab \
         --vocab-size $VOCAB_SIZE
 done
 for split in train dev test; do
-    for lang in ja en; do
+    for lang in $src_lang $tgt_lang; do
         python $REPO/scripts/spm_encode.py \
             --model ${data_dir}/prep/spm.$lang.nopretok.model \
                 < ${data_dir}/${split}.${lang} \
@@ -22,12 +19,15 @@ for split in train dev test; do
     done
 done
 
+src_lang="en"
+tgt_lang="ja"
+
 fairseq-preprocess \
-    --source-lang src --target-lang tgt \
+    --source-lang $src_lang --target-lang $tgt_lang \
     --trainpref ${data_dir}/prep/train.sp \
     --validpref ${data_dir}/pep/valid.sp \
     --testpref ${data_dir}/prep/test.sp \
-    --srcdict ${data_dir}/prep/dict.src.txt \
-    --tgtdict ${data_dir}/prep/dict.tgt.txt \
+    --srcdict ${data_dir}/prep/spm.${src_lang}.nopretok.vocab \
+    --tgtdict ${data_dir}/prep/spm.${tgt_lang}.nopretok.vocab\
     --destdir ${data_dir}/bin
 
