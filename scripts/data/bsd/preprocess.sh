a1f05624d303916fa45a0b43573d7faaee249e92
Sumire Honda <pu.sumirehonda@gmail.com>
2022-12-02 10:13:12 +0100

    variable position moved

--- scripts/data/bsd/preprocess.sh
+++ scripts/data/bsd/preprocess.sh
@@ -4,10 +4,13 @@ REPO=C:/Users/hsmr0/IM/main/contextual-mt
 data_dir=C:/Users/hsmr0/IM/main/contextual-mt/data/BSD-master/for_preprocess
 nmt_models_dir=C:/Users/hsmr0/IM/main/NMT_models
 
+src_lang="en"
+tgt_lang="ja"
+
 for lang in $src_lang $tgt_lang; do
     python $REPO/scripts/spm_train.py \
         ${data_dir}/train.${lang} \
-        --model-prefix ${data_dir}/prep/spm.${lang}.nopretok.model \ 
+        --model-prefix ${data_dir}/prep/spm.${lang}.nopretok.model \
         --vocab-file ${nmt_models_dir}/jparacrawl/${src_lang}-${tgt_lang}/small_${src_lang}-${tgt_lang}/dict.${lang}.txt \
         --vocab-size $VOCAB_SIZE
 done
@@ -15,20 +18,19 @@ for split in train dev test; do
     for lang in $src_lang $tgt_lang; do
         python $REPO/scripts/spm_encode.py \
             --model ${data_dir}/prep/spm.$lang.nopretok.model \
-                < ${data_dir}/${split}.${lang} \
-                > ${data_dir}/prep/${split}.sp.${lang}
-    done
-done
+            --inputs ${data_dir}/${split}.${lang} \
+            --outputs ${data_dir}/prep/${split}.sp.${lang} # with open('file', 'w') as sys.stdout: print('test')
 
-src_lang="en"
-tgt_lang="ja"
+    done
+
+done
 
 fairseq-preprocess \
     --source-lang $src_lang --target-lang $tgt_lang \
     --trainpref ${data_dir}/prep/train.sp \
     --validpref ${data_dir}/pep/valid.sp \
     --testpref ${data_dir}/prep/test.sp \
-    --srcdict ${nmt_models_dir}/jparacrawl/${src_lang}-${tgt_lang}/small_${src_lang}-${tgt_lang}/dict.${src_lang}.txt \ 
+    --srcdict ${nmt_models_dir}/jparacrawl/${src_lang}-${tgt_lang}/small_${src_lang}-${tgt_lang}/dict.${src_lang}.txt \
     --tgtdict ${nmt_models_dir}/jparacrawl/${src_lang}-${tgt_lang}/small_${src_lang}-${tgt_lang}/dict.${tgt_lang}.txt \
     --destdir ${data_dir}/bin
 
